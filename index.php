ba6fb0053e1589de7b46c79ab7af28ff4598225e
geraldopsan <geraldo@ifpi.edu.br>
2019-12-18 00:33:07 -0300

    parei na classe Mailerphp

--- index.php
+++ index.php
@@ -146,6 +146,24 @@ $app->post('/admin/users/:iduser', function($iduser){
 	exit;
 });
 
+$app->get('/admin/forgot', function(){
+
+	$page = new pageAdmin([
+
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot");
+});
+
+$app->post('/admin/forgot', function(){
+
+	 
+	$user = User::getForgot($_POST["email"]);
+
+});
+
 $app->run();
 
  ?>
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -9,6 +9,16 @@ class User extends Model {
 
 	const SESSION = "User";
 
+	const SECRET = "HcodePhp7_Secret";
+	
+	const SECRET_IV = "HcodePhp7_Secret_IV";
+	
+	const ERROR = "UserError";
+	
+	const ERROR_REGISTER = "UserErrorRegister";
+	
+	const SUCCESS = "UserSucesss";
+	
 	public function get($iduser)
 	{
 
@@ -134,9 +144,60 @@ class User extends Model {
 			":iduser"=>$this->getiduser()
 
 		));
-
 	}
 
+	public static function getForgot($email)
+	{
+
+		$sql = new Sql();
+		$results = $sql->select("SELECT * FROM tb_persons a INNER JOIN tb_users b USING(idperson) WHERE a.desemail = :email", array(
+
+			":email"=>$email
+		));
+
+		if (count($results) === 0)
+		{
+
+			throw new \Exception("Não foi possível recuperar a senha");
+		}
+		else
+		{
+
+			$data = $results[0];
+
+			$results2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
+
+				":iduser"=>$data["iduser"],
+				":desip"=>$_SERVER["REMOTE_ADDR"]
+			));
+
+			if (count($results2)===0)
+			{
+
+				throw new \Exception("Não foi possível recuperar a senha");
+			}
+
+			else
+			{
+
+				$dataRecovery = $results2[0];
+
+				$code = openssl_encrypt($dataRecovery['idrecovery'], 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
+
+				$code = base64_encode($code);
+				
+				if ($inadmin === true) {
+
+					$link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+				} 
+				else 
+				{
+
+					$link = "http://www.hcodecommerce.com.br/forgot/reset?code=$code";	
+				}	
+			}
+		}
+	}
 }
 
 ?>
